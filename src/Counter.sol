237b744cb8b6939db188ff469e2208fdf159ef2d
Erdal Tuncberk <98086202+0xSyntellect@users.noreply.github.com>
2024-07-11 15:14:06 +0300

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,13 +3,6 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin-contracts-4.9.0/access/Ownable.sol";
 
-//TODO: Deploy an app where users can interact with this smart contract. 
-//TODO: Display myNumber for the connected wallet. 
-//TODO: Display number
-//TODO: Display error message for unauthorized usage of setNumber()
-//TODO: Share the Github repo
-// Do not use any external APIs or backend. Everything should be done on Frontend!
-
 contract Counter is Ownable {
     uint256 private number;
 
